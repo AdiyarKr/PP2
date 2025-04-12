1dac0b4481509d38019029d8898a018e21c19b3b
Adiyar <Adi_karim@kbtu.kz>
2025-04-12 07:41:48 +0500

    upd

---
+++ lab10/querying_data.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+config = psycopg2.connect(
+    host = 'localhost',
+    database='phonebook',
+    user='postgres',
+    password='1234',
+    port = '5433'
+)
+
+current = config.cursor()#Этот инструмент позволяет нам взаимодействовать с данными в базе данных
+
+# запрос id, name, number по отсортированному имени 
+sql1 = '''
+    SELECT phone_number_id, name, surname, number_value FROM phone_number ORDER BY name ASC
+'''
+# запрос номера телефона определенного человека 
+sql2 = '''
+    SELECT number_value FROM phone_number WHERE name = ' Rina'
+'''
+sql3 = '''
+    SELECT * FROM phone_number WHERE surname=' Anuar'
+'''
+current.execute(sql2)
+
+final = current.fetchall()
+print(final)
+current.close()
+config.commit()
+config.close()
---
+++ lab10/user_table.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+config = psycopg2.connect(
+    host = 'localhost',
+    database='phonebook',
+    user='postgres',
+    password='1234',
+    port='5433'
+)
+cur = config.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    level INTEGER NOT NULL,
+    score INTEGER NOT NULL);
+""")
+
+cur.execute("""
+INSERT INTO users (username, level, score) 
+VALUES ('Rina', 0, 0), ('Shuaq', 0, 0);
+""")
+
+config.commit()
+
+cur.close()
+config.close()
